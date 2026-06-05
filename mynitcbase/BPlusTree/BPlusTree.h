8a724044f09bbec48c38ca9070bb1e2652009cd2
Aryan <aryan_b240149cs@nitc.ac.in>
2026-06-05 19:04:53 +0530

    Stage 12

--- mynitcbase/BPlusTree/BPlusTree.h
+++ mynitcbase/BPlusTree/BPlusTree.h
@@ -1,28 +1,26 @@
-#ifndef NITCBASE_BLOCKACCESS_H
-#define NITCBASE_BLOCKACCESS_H
+#ifndef NITCBASE_BPLUSTREE_H
+#define NITCBASE_BPLUSTREE_H
 
-#include "../BPlusTree/BPlusTree.h"
 #include "../Buffer/BlockBuffer.h"
-#include "../Cache/AttrCacheTable.h"
-#include "../Cache/RelCacheTable.h"
+#include "../Buffer/StaticBuffer.h"
+#include "../Cache/OpenRelTable.h"
 #include "../define/constants.h"
 #include "../define/id.h"
 
-class BlockAccess {
+class BPlusTree {
+ private:
+  static int findLeafToInsert(int rootBlock, Attribute attrVal, int attrType);
+  static int insertIntoLeaf(int relId, char attrName[ATTR_SIZE], int blockNum, Index entry);
+  static int splitLeaf(int leafBlockNum, Index indices[]);
+  static int insertIntoInternal(int relId, char attrName[ATTR_SIZE], int intBlockNum, InternalEntry entry);
+  static int splitInternal(int intBlockNum, InternalEntry internalEntries[]);
+  static int createNewRoot(int relId, char attrName[ATTR_SIZE], Attribute attrVal, int lChild, int rChild);
+
  public:
-  static int search(int relId, Attribute *record, char *attrName, Attribute attrVal, int op);
-
-  static int insert(int relId, union Attribute *record);
-
-  static int renameRelation(char *oldName, char *newName);
-
-  static int renameAttribute(char *relName, char *oldName, char *newName);
-
-  static int deleteRelation(char *relName);
-
-  static RecId linearSearch(int relId, char *attrName, Attribute attrVal, int op);
-
-  static int project(int relId, Attribute *record);
+  static int bPlusCreate(int relId, char attrName[ATTR_SIZE]);
+  static int bPlusInsert(int relId, char attrName[ATTR_SIZE], union Attribute attrVal, RecId recordId);
+  static RecId bPlusSearch(int relId, char attrName[ATTR_SIZE], union Attribute attrVal, int op);
+  static int bPlusDestroy(int rootBlockNum);
 };
 
-#endif  // NITCBASE_BLOCKACCESS_H
+#endif  // NITCBASE_BPLUSTREE_H
